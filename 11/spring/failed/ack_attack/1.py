b2eca684786d12cd1b3785dcd60a5853bf628d81
Alex Vondrak <alex@alex-pc>
2013-08-25 13:36:29 -0700

    11/spring: download contest data, set submission record straight

--- 11/spring/failed/ack_attack/1.py
+++ 11/spring/failed/ack_attack/1.py
@@ -8,21 +8,32 @@ def leaves(tree):
 
 copies_stored = {}
 
-def store_copy(person):
-    copies_stored.setdefault(person, 0)
-    copies_stored[person] += 1
-
 def send_petition(originator):
     node = originator
     path = []
     while petition_tree[node]:
-        path.push(petition_tree[node])
-        node = petition_tree[node]
+        if petition_tree[node]:
+            path.insert(0, petition_tree[node][0])
+        node = petition_tree[node][0]
+    ncopies = 1
+    for person in path[1:]:
+        ncopies += 1
+        copies_stored.setdefault(person, 0)
+        copies_stored[person] += ncopies
+    copies_stored[originator] = ncopies
+
+def dfs(person):
+    print '%d copies stored at %s' % (copies_stored.get(person, 0), person)
+    for underling in approval_tree[person]:
+        dfs(underling)
 
 def do_stuff():
     ultimate_authority = leaves(petition_tree)[0]
     petitioners = leaves(approval_tree) 
     for petitioner in petitioners:
+        send_petition(petitioner)
+    dfs(ultimate_authority)
+    print
 
 for line in sys.stdin.readlines():
     people = line.split()
