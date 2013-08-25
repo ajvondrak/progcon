ba8c683fdf8186114615a42a877201d08f485b96
Alex Vondrak <alex@alex-pc>
2013-08-24 23:04:47 -0700

    06/fall: reorganize

---
+++ 06/fall/failed/run_of_a_kind/1.py
@@ -0,0 +1,11 @@
+import re
+
+for input in raw_input().split('\n'): #'input' needs to be the entire line
+    input = ''.join(x for x in input.lower() if x in 'ht')
+    heads = max(re.findall('h*', input)) #find largest substring
+    tails = max(re.findall('t*', input))
+    input = input.replace(heads, heads.upper())
+    input = input.replace(tails, tails.upper())
+    print '\n', input
+    print 'longest run of heads is', `len(heads)`
+    print 'longest run of tails is', `len(tails)`, '\n'
