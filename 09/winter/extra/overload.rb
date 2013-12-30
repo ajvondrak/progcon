c7dc96d8bfb182b1e6b55827f0164998d400a817
Alex Vondrak <ajvondrak@gmail.com>
2013-12-29 18:13:02 -0800

    09/winter/extra: make rubocop happy

--- 09/winter/extra/overload.rb
+++ 09/winter/extra/overload.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # I've gotta brush up on Ruby & SQL, so I'm challenging myself to write a
 # programming contest solution in as close to pure SQL as I can, with some Ruby
 # plumbing as necessary.
@@ -10,9 +12,7 @@ def extends(db, sub, sup)
   insert = 'INSERT INTO hierarchy(subclass, superclass) VALUES (?1, ?2);'
   db.execute(insert, sub, sup)
   expand = 'SELECT DISTINCT ?1, superclass FROM hierarchy WHERE subclass = ?2;'
-  db.execute(expand, sub, sup) do |row|
-    extends(db, *row)
-  end
+  db.execute(expand, sub, sup) { |row| extends(db, *row) }
   extends(db, sub, sub)
   extends(db, sup, sup)
 end
@@ -22,9 +22,9 @@ def declare(db, return_type, method_name, formals)
   db.execute(insert, return_type, method_name)
   method_id = db.last_insert_row_id
   insert = 'INSERT INTO formal(method_id, type, offset) VALUES (?1, ?2, ?3);'
-  formals.each.with_index {|formal, offset|
+  formals.each.with_index do |formal, offset|
     db.execute(insert, method_id, formal, offset)
-  }
+  end
 end
 
 def declaration_parts(line)
@@ -39,32 +39,42 @@ def declaration(db, method_id)
   decl + "(#{db.execute(formals, method_id).join(', ')})"
 end
 
-def matches(db, context_type, method_name, actuals)
-  matches_method = <<SQL
+def matching_methods(db, context_type, method_name)
+  method_ids = <<SQL
 SELECT method.id
 FROM method, hierarchy
 WHERE method.name = ?2 AND
       method.return = hierarchy.subclass AND
       ?1 = hierarchy.superclass;
 SQL
+  db.execute(method_ids, context_type, method_name).flatten
+end
+
+def matches_formals(db, method_id, actuals)
   matches_formal = <<SQL
-SELECT 1
-FROM formal, hierarchy
+SELECT 1 FROM formal, hierarchy
 WHERE formal.method_id = ?1 AND
       formal.offset = ?3 AND
       formal.type = hierarchy.superclass AND
       ?2 = hierarchy.subclass;
 SQL
+  actuals.to_enum.with_index.all? do |actual, offset|
+    !db.execute(matches_formal, method_id, actual, offset).empty?
+  end
+end
+
+def matches_arg_count(db, method_id, actuals)
   count_formals = 'SELECT COUNT(offset) FROM formal WHERE method_id = ?'
-  db.execute(matches_method, context_type, method_name).flatten.reject {|id|
-    actuals.to_enum.with_index.any? {|actual, offset|
-      db.execute(matches_formal, id, actual, offset).empty?
-    } || db.get_first_value(count_formals, id) != actuals.length
-  }
+  db.get_first_value(count_formals, method_id) == actuals.length
+end
+
+def matches(db, context_type, method_name, actuals)
+  matching_methods(db, context_type, method_name).select do |id|
+    matches_arg_count(db, id, actuals) && matches_formals(db, id, actuals)
+  end
 end
 
 SQLite3::Database.new(':memory:') do |db|
-
   db.execute_batch(<<SQL)
 CREATE TABLE hierarchy (
   subclass TEXT,
@@ -89,13 +99,13 @@ CREATE TABLE formal (
 SQL
 
   hierarchy, declarations, invocations = $stdin.read.split(/\n\n/)
-  hierarchy.each_line {|line| extends(db, *line.chomp.split(/ extends /)) }
-  declarations.each_line {|line| declare(db, *declaration_parts(line)) }
-  invocations.each_line {|line|
+  hierarchy.each_line { |line| extends(db, *line.chomp.split(/ extends /)) }
+  declarations.each_line { |line| declare(db, *declaration_parts(line)) }
+  invocations.each_line do |line|
     puts "actual: #{line}"
-    matches(db, *declaration_parts(line)).each {|id|
+    matches(db, *declaration_parts(line)).each do |id|
       puts "formal: #{declaration(db, id)}"
-    }
+    end
     puts
-  }
+  end
 end
